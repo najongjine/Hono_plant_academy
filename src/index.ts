81a909993c9be25191d95532440e0ac3f63eca12
najongjine <najongjin3@hotmail.com>
2025-08-26 14:08:18 +0900

    .

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { cors } from "hono/cors";
+import * as dotenv from "dotenv";
 
-const app = new Hono()
+const app = new Hono();
+
+// .env.development 읽을건지, .env.production 읽을건지 결정
+const envFile =
+  process.env.NODE_ENV === "production"
+    ? ".env.production"
+    : ".env.development";
+dotenv.config({ path: envFile });
+
+app.use(
+  "*",
+  cors({
+    origin: "*",
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowHeaders: ["*"],
+  })
+);
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
@@ -9,7 +27,7 @@ app.get('/', (c) => {
 
 serve({
   fetch: app.fetch,
-  port: 3000
+  port: 3001
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
---
+++ src/router/gemini_route.ts
@@ -0,0 +1,23 @@
+import { Hono } from 'hono';
+
+const router = new Hono();
+
+
+router.get('/', async (c) => {
+    let result: { success: boolean; data: any; code: string; message: string } = {
+        success: true,
+        data: null,
+        code: "",
+        message: ``,
+    };
+    try {
+        const q = String(c.req.query("q") ?? "");
+        return c.json(result);
+    } catch (error: any) {
+        result.success = false;
+        result.message = `error. ${error?.message ?? ""}`
+        return c.json(result)
+    }
+});
+
+export default router;
